9acbab6c53645d69886d831b45860f914518c375
matveev96 <sasha.matveev1996@icloud.com>
2024-10-09 17:56:45 +0300

    feat: add header and main in project

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { Logo } from "../../components/logo/Logo";
+import { Menu } from "../../components/menu/Menu";
+import { Social } from '../../components/social/Social';
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo />
+            <Menu />
+            <Social/>
+        </StyledHeader>
+    )
+}
+
+const StyledHeader = styled.header`
+    background-color: #e3b1cb;
+    display: flex;
+`
