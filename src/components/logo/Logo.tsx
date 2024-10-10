745ca376675ea97dfddf1e73f26a25a25ee5b25b
matveev96 <sasha.matveev1996@icloud.com>
2024-10-10 20:52:43 +0300

    fix: change parametrs of logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import { Icon } from '../icon/Icon';
 export const Logo = () => {
     return (
         <a href="">
-            <Icon iconId={"logo"} height="80" width="80"/>
+            <Icon iconId={"logo"} height="80" width="80" viewBox="0 -20 100 100"/>
         </a>
     )
 }
