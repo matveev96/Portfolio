39998d8c739372c46cac0e44d1eb6d529c621e6f
matveev96 <sasha.matveev1996@icloud.com>
2024-10-10 10:56:44 +0300

    refactor: make Array with menuItems

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Menu = () => {
+export const Menu = (props: {menuItems: Array<string>}) => {
     return (
         <StyledMenu>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Tech Stack</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contacts</a></li>
+                {props.menuItems.map((item:string, index:number) => {
+                    return <li key={index}>
+                        <a href="">{item}</a>
+                    </li>
+                })}
             </ul>
         </StyledMenu>
     )
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,12 +4,14 @@ import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { Social } from '../../components/social/Social';
 
+const items = ["Home", "About", "Tech Stack", "Projects", "Contacts"];
+
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo />
-            <Menu />
-            <Social/>
+            <Menu menuItems={items} />
+            <Social />
         </StyledHeader>
     )
 }
