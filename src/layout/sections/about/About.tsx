5abfadd8c9f4ee719f398a0e823903ce2382ebdc
matveev96 <sasha.matveev1996@icloud.com>
2024-10-10 13:07:03 +0300

    fix: fix bug with bricksWhiteMode

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import styled from "styled-components";
-import { AboutComponent } from '../../../components/aboutComponent/AboutComponent';
+import { Card } from './card/Card';
 import { Icon } from '../../../components/icon/Icon';
 import { SectionTitle } from "../../../components/SectionTitle";
 import { SectionText } from "../../../components/SectionText";
+
 
 export const About = () => {
     return (
@@ -17,14 +18,14 @@ export const About = () => {
 
                     <StyledAboutSection>
                         <SectionTitle>Work Experience</SectionTitle>
-                        <AboutComponent position="Junior Web Developer" occupation="Full Time" organization="Dr. Rajkumar’s Learning App" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                        <AboutComponent position="Web Development Intern" occupation="Internship" organization="IonPixelz Web Solutions" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                        <AboutComponent position="SEO / SEM Specialist" occupation="Internship" organization="HAAPS" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                        <Card position="Junior Web Developer" occupation="Full Time" organization="Dr. Rajkumar’s Learning App" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                        <Card position="Web Development Intern" occupation="Internship" organization="IonPixelz Web Solutions" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                        <Card position="SEO / SEM Specialist" occupation="Internship" organization="HAAPS" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
                     </StyledAboutSection>
 
                     <StyledAboutSection>
                         <SectionTitle>Education</SectionTitle>
-                        <AboutComponent position="Bachelor in Electronics & Communication" occupation="Full Time" organization="Bangalore Instutute of Technology" period="Aug 2015 - Dec 2020"/>
+                        <Card position="Bachelor in Electronics & Communication" occupation="Full Time" organization="Bangalore Instutute of Technology" period="Aug 2015 - Dec 2020"/>
                         
                     </StyledAboutSection>
 
