6325da800a9e9f2763f3d04d11ae5ecd50a787f1
matveev96 <sasha.matveev1996@icloud.com>
2024-10-10 09:50:15 +0300

    refactor: add SectionTitle, SectionSubtitle and SectionText

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -3,28 +3,30 @@ import { FlexWrapper } from '../../../components/FlexWrapper';
 import styled from "styled-components";
 import { AboutComponent } from '../../../components/aboutComponent/AboutComponent';
 import { Icon } from '../../../components/icon/Icon';
+import { SectionTitle } from "../../../components/SectionTitle";
+import { SectionText } from "../../../components/SectionText";
 
 export const About = () => {
     return (
             <StyledAbout>
                 <FlexWrapper direction="column">
-                    <div>
-                        <h2>About Me</h2>
-                        <p>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</p>
-                    </div>
+                    <StyledAboutSection>
+                        <SectionTitle>About Me</SectionTitle>
+                        <SectionText>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</SectionText>
+                    </StyledAboutSection>
 
-                    <div>
-                        <h2>Work Experience</h2>
+                    <StyledAboutSection>
+                        <SectionTitle>Work Experience</SectionTitle>
                         <AboutComponent position="Junior Web Developer" occupation="Full Time" organization="Dr. Rajkumar’s Learning App" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
                         <AboutComponent position="Web Development Intern" occupation="Internship" organization="IonPixelz Web Solutions" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
                         <AboutComponent position="SEO / SEM Specialist" occupation="Internship" organization="HAAPS" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                    </div>
+                    </StyledAboutSection>
 
-                    <div>
-                        <h2>Education</h2>
+                    <StyledAboutSection>
+                        <SectionTitle>Education</SectionTitle>
                         <AboutComponent position="Bachelor in Electronics & Communication" occupation="Full Time" organization="Bangalore Instutute of Technology" period="Aug 2015 - Dec 2020"/>
                         
-                    </div>
+                    </StyledAboutSection>
 
                     <Icon iconId="bricksWhiteMode" width="591" height="1019" viewBox="0 0 591 1019"/>
                 </FlexWrapper>
@@ -35,4 +37,8 @@ export const About = () => {
 
 const StyledAbout = styled.section`
     background-color: #c8c899;
+`
+
+const StyledAboutSection = styled.div`
+    background-color: #96ba9a;
 `
