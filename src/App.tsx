4ef2331a361682715a65feebb472f057fe56e2bb
matveev96 <sasha.matveev1996@icloud.com>
2024-10-10 13:03:08 +0300

    feat: add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { About } from './layout/sections/about/About';
 import { Skills } from './layout/sections/skills/Skills';
+import { Projects } from './layout/sections/projects/Projects';
 
 
 
@@ -13,6 +14,7 @@ function App() {
             <Main/>
             <About/>
             <Skills/>
+            <Projects/>
 
         </div>
     );
