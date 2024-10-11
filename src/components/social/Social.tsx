228919a3b838212ed951ef3e27771ba33d413e43
matveev96 <sasha.matveev1996@icloud.com>
2024-10-11 17:29:31 +0300

    refactor: change div on ul

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -4,22 +4,33 @@ import styled from "styled-components";
 
 export const Social = () => {
     return (
-        <StyledSocial>
-            <a href="">
-                <Icon iconId={"githubMini"} height="30" width="30" viewBox="0 0 30 30"/>
-            </a>
-
-            <a href="">
-                <Icon iconId={"twitterMini"} height="30" width="30" viewBox="0 0 30 30"/>
-            </a>
-
-            <a href="">
-                <Icon iconId={"linkdInMini"} height="30" width="30" viewBox="0 0 30 30"/>
-            </a>
-        </StyledSocial>
+        <SocialList>
+            <SocialItem>
+                <SocialLink href="">
+                    <Icon iconId={"githubMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                </SocialLink>
+            </SocialItem>
+            <SocialItem>
+                <SocialLink href="">
+                    <Icon iconId={"twitterMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                </SocialLink>
+            </SocialItem>
+            <SocialItem>
+                <SocialLink href="">
+                    <Icon iconId={"linkdInMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                </SocialLink>
+            </SocialItem>
+        </SocialList>
     )
 }
 
-const StyledSocial = styled.div`
+const SocialList = styled.ul`
     display: flex;
+`
+
+const SocialItem = styled.li`
+    
+`
+const SocialLink = styled.a`
+    
 `
