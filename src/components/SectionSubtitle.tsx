57ef668639e7c4b73e0b7bc484a9b7bbcf0e35a7
matveev96 <sasha.matveev1996@icloud.com>
2024-10-15 11:08:20 +0300

    refactor: refactor project

--- src/components/SectionSubtitle.tsx
+++ src/components/SectionSubtitle.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components";
 
 export const SectionSubtitle = styled.h3`
     color: blue;
-
+    text-align: center;
 `
--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { Menu } from "../../components/menu/Menu";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contacts"];
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <Menu menuItems={items}/>
-            <Copyright>Designed and built by <ColorText>Pavan MG</ColorText> with <ColorText>Love</ColorText> & <ColorText>Coffee</ColorText></Copyright>
+            <FlexWrapper justfy={"space-between"} align={"center"}>
+                <Menu menuItems={items}/>
+                <Copyright>Designed and built by <ColorText>Pavan MG</ColorText> with <ColorText>Love</ColorText> & <ColorText>Coffee</ColorText></Copyright>
+            </FlexWrapper>
         </StyledFooter>
     )
 }
 
 const StyledFooter = styled.footer`
-    display: flex;
-    justify-content: space-between;
 `
 
 const Copyright = styled.small`
