88ea40c2fcd20e1676b508377039f25ce0b298d5
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 15:57:34 +0300

    feat: add styles for menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { theme } from '../../styles/Theme';
 
-export const Menu = (props: {menuItems: Array<string>}) => {
+type MenuPropsType = {
+    menuItems: Array<string>,
+    variant: 'header' | 'footer' | 'darkMode'
+}
+
+export const Menu = (props: MenuPropsType) => {
     return (
-        <StyledMenu>
+        <StyledMenu variant={props.variant}>
             <ul>
-                {props.menuItems.map((item:string, index:number) => {
+                {props.menuItems.map((item: string, index: number) => {
                     return <li key={index}>
-                        <a href="">{item}</a>
+                        <a href="#">{item}</a>
                     </li>
                 })}
             </ul>
@@ -15,9 +21,32 @@ export const Menu = (props: {menuItems: Array<string>}) => {
     )
 }
 
-const StyledMenu = styled.nav`
+const StyledMenu = styled.nav<{ variant: 'header' | 'footer' | 'darkMode' }>`
+    max-width: 609px;
+    width: 100%;
     ul {
         display: flex;
-        gap: 30px;
+        justify-content: space-between;
     }
+
+    ${(props) =>
+        props.variant === "header" && css`
+        a {
+            color: ${theme.colors.font.darkContent};
+        }
+    `}
+
+    ${(props) =>
+        props.variant === "footer" && css`
+      a {
+        color: ${theme.colors.font.darkTitle};
+      }
+    `}
+
+    ${(props) =>
+        props.variant === "darkMode" && css`
+      a {
+        color: ${theme.colors.font.lightContent};
+      }
+    `}
 `
