88adcc259f36924897b750792879886224c6a051
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 15:57:07 +0300

    feat: add syles for logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Icon } from '../icon/Icon';
+import styled from "styled-components";
 
 type LogoPropsType = {
     color?: string,
@@ -8,8 +9,13 @@ type LogoPropsType = {
 
 export const Logo = (props:LogoPropsType) => {
     return (
-        <a href="#">
-            <Icon iconId={props.variation || "logo"} height="80" width="80" viewBox="0 -20 100 100" color={props.color}/>
-        </a>
+        <StyledLogo href="#">
+            <Icon iconId={props.variation || "logo"} height="59" width="97" viewBox="0 0 100 60" color={props.color}/>
+        </StyledLogo>
     )
 }
+
+const StyledLogo = styled.a`
+    display: inline-block;
+    height: 59px;
+`
