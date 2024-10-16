52d232ef02e66f39158221c9c76bcb0104d85986
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 23:33:59 +0300

    feat: add styles to About secton

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -5,41 +5,76 @@ import { Card } from './card/Card';
 import { Icon } from '../../../components/icon/Icon';
 import { SectionTitle } from "../../../components/SectionTitle";
 import { SectionText } from "../../../components/SectionText";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 
 export const About = () => {
     return (
             <StyledAbout>
-                <FlexWrapper direction="column">
-                    <StyledAboutSection>
-                        <SectionTitle>About Me</SectionTitle>
-                        <SectionText>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</SectionText>
-                    </StyledAboutSection>
+                <Container>
+                    <FlexWrapper >
+                        <AboutWrapper>
+                            <StyledSection>
+                                <Title>About Me</Title>
+                                <SectionText>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</SectionText>
+                            </StyledSection>
 
-                    <StyledAboutSection>
-                        <SectionTitle>Work Experience</SectionTitle>
-                        <Card position="Junior Web Developer" occupation="Full Time" organization="Dr. Rajkumar’s Learning App" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                        <Card position="Web Development Intern" occupation="Internship" organization="IonPixelz Web Solutions" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                        <Card position="SEO / SEM Specialist" occupation="Internship" organization="HAAPS" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
-                    </StyledAboutSection>
+                            <StyledSection>
+                                <Title>Work Experience</Title>
+                                <Card margin="30px" position="Junior Web Developer" occupation="Full Time" organization="Dr. Rajkumar’s Learning App" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                                <Card margin="30px" position="Web Development Intern" occupation="Internship" organization="IonPixelz Web Solutions" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                                <Card position="SEO / SEM Specialist" occupation="Internship" organization="HAAPS" city="Bengaluru" period="Sep 2021 - Dec 2021"/>
+                            </StyledSection>
 
-                    <StyledAboutSection>
-                        <SectionTitle>Education</SectionTitle>
-                        <Card position="Bachelor in Electronics & Communication" occupation="Full Time" organization="Bangalore Instutute of Technology" period="Aug 2015 - Dec 2020"/>
+                            <StyledSection>
+                                <Title>Education</Title>
+                                <Card position="Bachelor in Electronics & Communication" occupation="Full Time" organization="Bangalore Instutute of Technology" display="none" period="Aug 2015 - Dec 2020"/>
+                                
+                            </StyledSection>
+                        </AboutWrapper>
+
+                        <AbstractFigure>
+                            <Icon iconId="bricksWhiteMode" width="591" height="1019" viewBox="0 0 591 1019"/>
+                        </AbstractFigure>
                         
-                    </StyledAboutSection>
-
-                    <Icon iconId="bricksWhiteMode" width="591" height="1019" viewBox="0 0 591 1019"/>
-                </FlexWrapper>
+                        
+                    </FlexWrapper>
+                </Container>
             </StyledAbout>
 
     )
 }
 
+
 const StyledAbout = styled.section`
-    background-color: #c8c899;
+    position: relative;
 `
 
-const StyledAboutSection = styled.div`
-    background-color: #96ba9a;
+const AboutWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 710px;
+    width: 100%;
+`
+
+const StyledSection = styled.div`
+    margin-bottom: 38px;
+`
+
+const Title = styled.h3`
+    font-weight: 700;
+    font-size: 4.2rem;
+    line-height: 124%;
+    color: ${theme.colors.font.darkTitle};
+    margin-bottom: 3.8rem;
+
+`
+
+const AbstractFigure = styled.div`
+    position: absolute;
+    content: "";
+    left: 1000px;
+    top: -150px;
+
 `
