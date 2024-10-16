f097e18ef9f42ce2f869cd8df91705cc33460cad
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 15:58:08 +0300

    feat: add styles for Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,20 +3,42 @@ import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { Social } from '../../components/social/Social';
+import { Container } from "../../components/Container";
+import { FlexWrapper } from "../../components/FlexWrapper";
+import { theme } from "../../styles/Theme";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contacts"];
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-            <Menu menuItems={items} />
-            <Social />
+            <Container>
+                <FlexWrapper justfy="space-between"  wrap="wrap">
+                    <Logo variation={'logoGradient'} />
+                    <MenuBox>
+                        <Menu menuItems={items} variant={"header"}/>
+                        <Social color={theme.colors.font.darkContent}/>
+                    </MenuBox>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     )
 }
 
 const StyledHeader = styled.header`
-    background-color: #e3b1cb;
+    padding-top: 41px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 9999;
+`
+
+const MenuBox = styled.div`
     display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    max-width: 790px;
+    width: 100%;
+    padding-top: 8px;
 `
