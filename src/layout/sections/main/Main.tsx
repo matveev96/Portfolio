b7ba4bf52c26d027f8f387e6c5eb165eca2a3639
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 17:42:32 +0300

    feat: add styles for main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,44 +2,83 @@ import React from "react";
 import styled from "styled-components";
 import photo from "../../../assets/images/avatar.webp"
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { theme } from "../../../styles/Theme";
+import { Container } from "../../../components/Container";
 
 
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper justfy="space-around" wrap="wrap" align="center">
-                <div>
-                    <MainText>Hi 👋,</MainText>
-                    <MainText>My name is</MainText>
-                    <Name>Aliaksandr Matsveyeu</Name>
-                    <MainTitle>I build things for web</MainTitle>
-                </div>
-
-                <Photo src={photo}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper justfy="space-between" wrap="wrap" align="center">
+                    <TextWrapper>
+                        <MainText>Hi 👋,</MainText>
+                        <MainText>My name is</MainText>
+                        <Name>PavanMG</Name>
+                        <MainTitle>I build things for web</MainTitle>
+                    </TextWrapper>
+                    <PhotoWrapper></PhotoWrapper>
+                </FlexWrapper>
+            </Container>
+            
             
         </StyledMain>
     )
 }
 
 const StyledMain = styled.div`
-    background-color: #8dbfbf;
+    display: flex;
+    min-height: 870px;
 `
-
-const Photo = styled.img`
-    width: 350px;
-    height: 350px;
-    object-fit: cover;
+const TextWrapper = styled.div`
+    max-width: 636px;
+    width: 100%;
+    min-height: 297px;
+    font-weight: 700;
+    font-size: 5.8rem;
+    line-height: 121%;
+    color: ${theme.colors.font.darkTitle};
 `
 
 const Name = styled.h2`
-    
+    font-size: 5.8rem;
+    background: ${theme.colors.font.gradient};
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `
 
 const MainTitle = styled.h1`
+    font-size: 5.8rem;
 `
 
-const MainText = styled.span`
+const MainText = styled.p`
     
 `
+
+const PhotoWrapper = styled.div`
+    position: relative;
+    border-radius: 230px;
+    width: 349px;
+    height: 349px;
+    background-image: url(${photo});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+
+    &::before {
+        content: "";
+        position: absolute;
+        top: -9px;
+        bottom: -9px;
+        right: -9px;
+        left: -9px;
+        background: ${theme.colors.font.gradient};
+        border-radius: 230px;
+        z-index: -1;
+    }
+`
+
+
