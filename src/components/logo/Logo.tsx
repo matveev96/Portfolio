7a4490a1a94838b7cbc4cf1ba8488d1e54c19e83
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 13:29:54 +0300

    feat: add props for color and variation of icon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Icon } from '../icon/Icon';
 
-export const Logo = () => {
+type LogoPropsType = {
+    color?: string,
+    variation?: 'logoGradient'
+}
+
+export const Logo = (props:LogoPropsType) => {
     return (
-        <a href="">
-            <Icon iconId={"logo"} height="80" width="80" viewBox="0 -20 100 100"/>
+        <a href="#">
+            <Icon iconId={props.variation || "logo"} height="80" width="80" viewBox="0 -20 100 100" color={props.color}/>
         </a>
     )
 }
