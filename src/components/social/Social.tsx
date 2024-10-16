38dd2f1c692746dbffbd5a6206bb1d0535bbb7ca
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 18:16:01 +0300

    fix: change viewBox

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -2,22 +2,26 @@ import React from "react";
 import { Icon } from '../icon/Icon';
 import styled from "styled-components";
 
-export const Social = () => {
+type SocialPropsType = {
+    color: string
+}
+
+export const Social = (props:SocialPropsType) => {
     return (
         <SocialList>
             <SocialItem>
                 <SocialLink href="">
-                    <Icon iconId={"githubMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                    <Icon iconId={"githubMini"} height="30" width="30" viewBox="0 0 30 32" color={props.color}/>
                 </SocialLink>
             </SocialItem>
             <SocialItem>
                 <SocialLink href="">
-                    <Icon iconId={"twitterMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                    <Icon iconId={"twitterMini"} height="30" width="30" viewBox="0 0 30 32" color={props.color}/>
                 </SocialLink>
             </SocialItem>
             <SocialItem>
                 <SocialLink href="">
-                    <Icon iconId={"linkdInMini"} height="30" width="30" viewBox="0 0 30 30"/>
+                    <Icon iconId={"linkdInMini"} height="30" width="30" viewBox="0 0 30 30" color={props.color}/>
                 </SocialLink>
             </SocialItem>
         </SocialList>
@@ -26,6 +30,8 @@ export const Social = () => {
 
 const SocialList = styled.ul`
     display: flex;
+    justify-content: space-between;
+    gap: 20px;
 `
 
 const SocialItem = styled.li`
