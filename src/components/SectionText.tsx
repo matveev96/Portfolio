441a6b16fb09e4d9bf342de98bfdf3b7868c56a2
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 23:30:07 +0300

    feat: add props type

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
+type TextPropsType = {
+    color?: string,
+    weight?: string
+}
 
-export const SectionText = styled.p`
-    color: #552828;
+export const SectionText = styled.p<TextPropsType>`
+    color: ${props => props.color};
+    font-weight: ${props => props.weight || "400"};
+    font-size: 1.8rem;
+    line-height: 144%;
 `
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
+type TitlePropsType = {
+        align?: string,
+        color?: string,
+        fontSize?: string
+}
 
-export const SectionTitle = styled.h2`
-        color: red;
-        text-align: center;
+export const SectionTitle = styled.h2<TitlePropsType>`
+        color: ${props => props.color};
+        text-align: ${props => props.align || "center"};
+        font-size: ${props => props.fontSize};
 `
