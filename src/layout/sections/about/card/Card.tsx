c24193f603f1bfd184cbd21f2601ec2bd3a2021e
matveev96 <sasha.matveev1996@icloud.com>
2024-10-16 23:34:36 +0300

    feat: add styles to Card in About section

--- src/layout/sections/about/card/Card.tsx
+++ src/layout/sections/about/card/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Icon } from '../../../../components/icon/Icon';
+import { theme } from "../../../../styles/Theme";
 
 type CardPropsType = {
     position: string,
@@ -8,41 +9,99 @@ type CardPropsType = {
     organization: string,
     city?: string,
     period: string,
+    display?: "none",
+    margin?: string
 }
 
-export const Card = (props:CardPropsType) => {
+export const Card = (props: CardPropsType) => {
     return (
-        <div>
-            <StyledDiv>
-                <h3>{props.position || "null"}</h3>
-                <span>{props.occupation || "null"}</span>
-            </StyledDiv>
-            
-            <StyledDiv>
-                <StyledIconSpanContainer>
-                    <Icon iconId={"building"} height="12" width="16" viewBox="0 0 16 12"/>
-                    <span>{props.organization || "null"}</span>
-                </StyledIconSpanContainer>
-                
-                <StyledIconSpanContainer>
-                    <Icon iconId={"map"} height="12" width="16" viewBox="0 0 16 12"/>
-                    <span>{props.city}</span>
-                </StyledIconSpanContainer>
+        <CardStyle margin={props.margin}>
+            <CardContainerTop>
+                <Position>{props.position || "null"}</Position>
+                <Occupation>{props.occupation || "null"}</Occupation>
+            </CardContainerTop>
 
-                <StyledIconSpanContainer>
-                    <Icon iconId={"calendar"} height="12" width="16" viewBox="0 0 16 12"/>
-                    <span>{props.period || "null"}</span> 
-                </StyledIconSpanContainer>
-            </StyledDiv>
-        </div>
+            <CardContainerBottom>
+                <InfoContainer>
+                    <Icon iconId={"building"} height="12" width="16" viewBox="0 0 16 12" />
+                    <Info>{props.organization || "null"}</Info>
+                </InfoContainer>
+
+                <InfoContainer display={props.display}>
+                    <Icon iconId={"map"} height="12" width="16" viewBox="0 0 16 12" />
+                    <Info>{props.city}</Info>
+                </InfoContainer>
+
+                <InfoContainer>
+                    <Icon iconId={"calendar"} height="12" width="16" viewBox="0 0 16 12" />
+                    <Info>{props.period || "null"}</Info>
+                </InfoContainer>
+            </CardContainerBottom>
+        </CardStyle>
     )
 }
 
-const StyledDiv = styled.div`
-    display: flex;
-    justify-content: space-between;
+
+const CardStyle = styled.div<{margin?: string}>`
+    border-bottom: 2px solid ${theme.colors.borderBg};
+    margin-bottom: ${props => props.margin};
+    padding-bottom: 20px;
+
 `
 
-const StyledIconSpanContainer = styled.div`
+const CardContainerTop = styled.div`
     display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
 `
+
+const CardContainerBottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    & :nth-of-type(3){
+        justify-content: flex-end;
+    }
+`
+
+const InfoContainer = styled.div<{display?: "none"}>`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    flex: 1 1 33%;
+
+    ${props => props.display === "none" && css`
+        display: none;
+    `}
+
+
+`
+
+const Info = styled.span`
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 233%;
+    letter-spacing: 0.08em;
+    color: ${theme.colors.font.lightContent};
+`
+
+const Position = styled.h4`
+    font-weight: 400;
+    font-size: 2rem;
+    line-height: 140%;
+    letter-spacing: 0.05em;
+    `
+
+const Occupation = styled.span`
+    border-radius: 100px;
+    width: 84px;
+    height: 24px;
+    background-color: ${theme.colors.occupationBg};
+    color: ${theme.colors.font.occupationFont};
+    font-weight: 600;
+    font-size: .9rem;
+    line-height: 289%;
+    text-align: center;
+`
+
