7d11fa932d61de901cb21c96d8ae8e8319f22777
matveev96 <sasha.matveev1996@icloud.com>
2024-10-17 23:16:12 +0300

    feat: add styles to Projects component

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -4,32 +4,34 @@ import { SectionTitle } from "../../../components/SectionTitle";
 import { SectionSubtitle } from "../../../components/SectionSubtitle";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Project } from "./project/Project";
-import projectOne from "../../../assets/images/project_1.webp"
-import projectTwo from "../../../assets/images/project_2.webp"
-import projectThree from "../../../assets/images/project_3.webp"
-import projectFour from "../../../assets/images/project_4.webp"
-import projectFive from "../../../assets/images/project_5.webp"
-import projectSix from "../../../assets/images/project_6.webp"
+import projectOne from "../../../assets/images/project_1.webp";
+import projectTwo from "../../../assets/images/project_2.webp";
+import projectThree from "../../../assets/images/project_3.webp";
+import projectFour from "../../../assets/images/project_4.webp";
+import projectFive from "../../../assets/images/project_5.webp";
+import projectSix from "../../../assets/images/project_6.webp";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Projects = () => {
     return (
         <StyledProjects>
-            <SectionTitle>Projects</SectionTitle>
-            <SectionSubtitle>Things I’ve built so far</SectionSubtitle>
-            <FlexWrapper wrap={"wrap"} justfy="space-between">
-                <Project src={projectOne} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-                <Project src={projectTwo} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-                <Project src={projectThree} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-                <Project src={projectFour} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-                <Project src={projectFive} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-                <Project src={projectSix} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
-
-            </FlexWrapper>
-
+            <Container>
+                <SectionTitle align="center" fontSize="4.8rem" color={theme.colors.font.darkTitle} marginBtm="50px">Projects</SectionTitle>
+                <SectionSubtitle fontSize="3.2rem" align="center" color={theme.colors.font.darkContent} marginBtm="130px">Things I’ve built so far</SectionSubtitle>
+                <FlexWrapper wrap={"wrap"} justfy="space-between" gap="34px">
+                    <Project src={projectOne} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                    <Project src={projectTwo} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                    <Project src={projectThree} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                    <Project src={projectFour} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                    <Project src={projectFive} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                    <Project src={projectSix} title={"Project Tile goes here"} list={"HTML , JavaScript, SASS, React"} text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} />
+                </FlexWrapper>
+            </Container>
         </StyledProjects>
     )
 }
 
-const StyledProjects = styled.div`
-    background-color: #abc6cf;
+const StyledProjects = styled.section`
+    
 `
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { SectionSubtitle } from "../../../../components/SectionSubtitle";
 import { SectionText } from "../../../../components/SectionText";
 import { Icon } from "../../../../components/icon/Icon";
+import { theme } from "../../../../styles/Theme";
 
 type ProjectPropsType = {
     title: string,
@@ -14,50 +15,83 @@ type ProjectPropsType = {
 export const Project = (props: ProjectPropsType) => {
     return (
         <StyledProject>
-            <Image src={props.src}/>
-            <SectionSubtitle>{props.title}</SectionSubtitle>
-            <SectionText>{props.text}</SectionText>
-            <StyledStackList>Tech stack: <StyledStack>{props.list}</StyledStack> </StyledStackList>
+            <ImageBox>
+                <Image src={props.src}/>
+            </ImageBox>
 
-            <StyledLinkContainer>
-                <StyledLinkContainer>
-                    <Icon iconId={"chain"} height="20" width="20" viewBox="0 0 20 20"/>
-                    <Link href="#">Live Preview</Link>
-                </StyledLinkContainer>
-                
-                <StyledLinkContainer>
-                    <Icon iconId={"githubMini"} height="20" width="20" viewBox="0 0 30 30"/>
-                    <Link href="#">View Code</Link>
-                </StyledLinkContainer>
-            </StyledLinkContainer>
+            <ContentBox>
+                <SectionSubtitle weight="500" color={theme.colors.font.black} fontSize="2.8rem" align="left" marginBtm="17px">{props.title}</SectionSubtitle>
+                <SectionText weight="300" color={theme.colors.font.darkContent} marginBtm="12px">{props.text}</SectionText>
+                <Stack>Tech stack: <StyledStackList>{props.list}</StyledStackList></Stack>
 
+                <LinkBox>
+                    <LinkItem>
+                        <Icon iconId={"chain"} height="20" width="20" viewBox="0 0 20 20" color="transparent"/>
+                        <Link href="#">Live Preview</Link>
+                    </LinkItem>
+                    
+                    <LinkItem>
+                        <Icon color={theme.colors.font.black} iconId={"githubMini"} height="20" width="20" viewBox="0 0 30 30"/>
+                        <Link href="#">View Code</Link>
+                    </LinkItem>
+                </LinkBox>
+            </ContentBox>
         </StyledProject>
     )
 }
 
-const StyledProject = styled.section`
-    background-color: beige;
-    width: 30%;
-    margin: 5px;
+const StyledProject = styled.div`
+    box-shadow: 2px 2px 100px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 20px;
+    width: 375px;
+`
+
+const ImageBox = styled.div`
+    height: 260px;
+    margin-bottom: 27px;
+`
+
+const ContentBox = styled.div`
+    width: 315px;
+    margin: 0 auto;
 `
 
 const Image = styled.img`
-width: 100%;
-height: 260px;
-object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+`
+
+const StyledStackList = styled.span`
+    font-weight: 300;
+    font-size: 1.4rem; 
+`
+
+const Stack = styled.h4`
+    color: ${theme.colors.font.darkTitle};
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 162%;
+    margin-bottom: 20px;
+`
+
+const LinkBox = styled.div`
+    display: flex;
+    gap: 48px;
+    margin-bottom: 25px;
+`
+
+const LinkItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
 `
 
 const Link = styled.a`
-    
-`
-
-const StyledStackList = styled.p`
-`
-
-const StyledStack = styled.span`
-`
-
-const StyledLinkContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 162%;
+    text-decoration: underline;
+    color: ${theme.colors.font.black};
 `
