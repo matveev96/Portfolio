b139c3fd1f352c3e219a256371f2c08f1083f236
matveev96 <sasha.matveev1996@icloud.com>
2024-10-17 16:47:32 +0300

    feat: add styles to Skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,32 +4,35 @@ import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from "../../../components/SectionTitle";
 import { SectionSubtitle } from "../../../components/SectionSubtitle";
 import { Skill } from "./skill/Skill";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>My Tech Stack</SectionTitle>
-            <SectionSubtitle> Technologies I’ve been working with recently</SectionSubtitle>
-            <FlexWrapper wrap={"wrap"} justfy={"space-between"}>
-                <Skill iconId={"html"}/>
-                <Skill iconId={"css"}/>
-                <Skill iconId={"js"}/>
-                <Skill iconId={"react"}/>
-                <Skill iconId={"vector"}/>
-                <Skill iconId={"bootstrap"}/>
-                <Skill iconId={"tailwind"}/>
-                <Skill iconId={"sass"}/>
-                <Skill iconId={"git"}/>
-                <Skill iconId={"greenshock"}/>
-                <Skill iconId={"vsCode"}/>
-                <Skill iconId={"github"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle align="center" fontSize="4.8rem" color={theme.colors.font.darkTitle} marginBtm="50px">My Tech Stack</SectionTitle>
+                <SectionSubtitle fontSize="3.2rem" align="center" color={theme.colors.font.darkContent} marginBtm="130px"> Technologies I’ve been working with recently</SectionSubtitle>
+                <FlexWrapper wrap={"wrap"}  justfy="center" gap="90px">
+                    <Skill iconId={"html"}/>
+                    <Skill iconId={"css"}/>
+                    <Skill iconId={"js"}/>
+                    <Skill iconId={"react"} viewBox="-5 -5 120 110"/>
+                    <Skill iconId={"vector"} viewBox="-5 -5 120 110"/>
+                    <Skill iconId={"bootstrap"} viewBox="-5 -5 100 100"/>
+                    <Skill iconId={"tailwind"} viewBox="10 10 120 110"/>
+                    <Skill iconId={"sass"} viewBox="-10 -5 120 110"/>
+                    <Skill iconId={"git"} viewBox="-5 0 120 110"/>
+                    <Skill iconId={"greenshock"}/>
+                    <Skill iconId={"vsCode"}/>
+                    <Skill iconId={"github"} viewBox="-5 -5 100 100"/>
+                </FlexWrapper>
+            </Container>
         </StyledSkills>
     )
 }
 
 const StyledSkills = styled.section`
-    /* min-height: 100vh; */
-    background-color: #cac5ce;
+    height: 100vh;
 `
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { Icon } from "../../../../components/icon/Icon";
+import { FlexWrapper } from "../../../../components/FlexWrapper";
 
 type SkillPropsType = {
     iconId: string,
+    viewBox?: string
 }
 
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <Icon iconId={props.iconId}/>
+            <Icon iconId={props.iconId} viewBox={props.viewBox} width="110px" height="110px"/>
         </StyledSkill>
     )
 }
 
 const StyledSkill = styled.div`
-    margin: 5px;
-    width: 15%;
-    background-color: antiquewhite;
+    width: 120px;
     display: flex;
+    align-items: center;
     justify-content: center;
 `
