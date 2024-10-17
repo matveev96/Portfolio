2f4d4f16a821c8fb27f78f43e0323bddbac99c45
matveev96 <sasha.matveev1996@icloud.com>
2024-10-17 16:49:33 +0300

    feat: add styles and props to Subtitle

--- src/components/SectionSubtitle.tsx
+++ src/components/SectionSubtitle.tsx
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
-export const SectionSubtitle = styled.h4`
-    color: blue;
-    text-align: center;
+type SubtitlePropsType = {
+    align?: string,
+    color?: string,
+    fontSize?: string,
+    marginBtm?: string
+}
+
+export const SectionSubtitle = styled.h4<SubtitlePropsType>`
+    font-weight: 400;
+    color: ${props => props.color};
+    text-align: ${props => props.align || "center"};
+    font-size: ${props => props.fontSize};
+    margin-bottom: ${props => props.marginBtm};
 `
