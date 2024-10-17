b82eb28b16f0c02291c1ccf6d3083098d9b5baf1
matveev96 <sasha.matveev1996@icloud.com>
2024-10-17 16:50:24 +0300

    feat: add styles and props to Title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,11 +3,14 @@ import styled from "styled-components";
 type TitlePropsType = {
         align?: string,
         color?: string,
-        fontSize?: string
+        fontSize?: string,
+        marginBtm?: string
 }
 
 export const SectionTitle = styled.h2<TitlePropsType>`
+        font-weight: 700;
         color: ${props => props.color};
         text-align: ${props => props.align || "center"};
         font-size: ${props => props.fontSize};
+        margin-bottom: ${props => props.marginBtm};
 `
