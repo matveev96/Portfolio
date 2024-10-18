2d5e93579e2e67de3b1d05389ad5e15adbdf6c49
matveev96 <sasha.matveev1996@icloud.com>
2024-10-18 23:40:50 +0300

    refactor: refactoring Menu and add effects to hover

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,25 +4,43 @@ import { theme } from '../../styles/Theme';
 
 type MenuPropsType = {
     menuItems: Array<string>,
-    variant: 'header' | 'footer' | 'darkMode'
+    weight?: string,
+    size?: string,
+    colorMask?: string,
+    colorMaskHover?: string,
+    colorMaskLine?: string
 }
 
 export const Menu = (props: MenuPropsType) => {
     return (
-        <StyledMenu variant={props.variant}>
+        <StyledMenu>
             <ul>
                 {props.menuItems.map((item: string, index: number) => {
-                    return <li key={index}>
-                        <a href={`#${index}`}>{item}</a>
-                    </li>
+                    return <ListItem key={index} colorMaskLine={props.colorMaskLine} colorMaskHover={props.colorMaskHover}>
+                        <Link size={props.size} weight={props.weight} href={`#${index}`}>
+                                {item}
+                            <Mask colorMask={props.colorMask}>
+                                <span>{item}</span>
+                            </Mask>
+                            <Mask colorMask={props.colorMask}>
+                                <span>{item}</span>
+                            </Mask>
+                        </Link>
+                    </ListItem>
                 })}
             </ul>
         </StyledMenu>
     )
 }
 
-const StyledMenu = styled.nav<{ variant: 'header' | 'footer' | 'darkMode' }>`
-    max-width: 609px;
+const Link = styled.a<{weight?: string, size?: string}>`
+    color: transparent;
+    font-weight: ${props => props.weight};
+    font-size: ${props => props.size};
+`
+
+const StyledMenu = styled.nav`
+    max-width: 610px;
     width: 100%;
     ul {
         display: flex;
@@ -30,32 +48,57 @@ const StyledMenu = styled.nav<{ variant: 'header' | 'footer' | 'darkMode' }>`
         font-family: 'DM Sans', sans-serif;
     }
 
-    ${(props) =>
-        props.variant === "header" && css`
-        a {
-            color: ${theme.colors.font.darkContent};
-            font-weight: 500;
-            font-size: 2rem;
-            line-height: 130%;
-            text-align: center;
+`
+
+const Mask = styled.span<{colorMask?: string}>`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    height: 50%;
+    overflow-y: hidden;
+    color: ${props => props.colorMask};
+    transition: .2s all ease-in;
+    
+    & + & {
+        top: 50%;
+        span {
+            display: inline-block;
+            transform: translateY(-50%);
         }
-    `}
+    }
+`
 
-    ${(props) =>
-        props.variant === "footer" && css`
-      a {
-        font-family: 'DM Sans';
-        color: ${theme.colors.font.darkTitle};
-        font-weight: 400;
-        font-size: 1.8rem;
-        line-height: 144%;
-      }
-    `}
+const ListItem = styled.li<{colorMaskLine?: string, colorMaskHover?: string}>`
+    position: relative;
 
-    ${(props) =>
-        props.variant === "darkMode" && css`
-      a {
-        color: ${theme.colors.font.lightContent};
-      }
-    `}
+    &::before {
+        content: "";
+        display: inline-block;
+        height: 2px;
+        background-color: ${props => props.colorMaskLine};
+
+        position: absolute;
+        top: 50%;
+        left: -10px;
+        right: -10px;
+        z-index: 1;
+
+        transform: scale(0);
+    }
+
+    &:hover {
+        &::before {
+            transform: scale(1);
+        }
+
+        ${Mask} {
+            transform: skewX(12deg) translateX(5px);
+            color: ${props => props.colorMaskHover};
+            & + ${Mask} {
+            transform: skewX(12deg) translateX(-5px);
+            }
+        }
+    }
+
 `
