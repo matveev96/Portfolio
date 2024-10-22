44ff8aac1fdeb5a50ef396849c16305c40cbf252
matveev96 <sasha.matveev1996@icloud.com>
2024-10-22 13:45:34 +0300

    feat: add media vars

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -21,5 +21,10 @@ export const theme = {
             black: "#000"
         }
         
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)"
     }
 }
