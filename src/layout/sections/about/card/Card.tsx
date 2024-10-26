2b552830c0c135dea2dce161edcd917b64391c4f
matveev96 <sasha.matveev1996@icloud.com>
2024-10-26 23:37:48 +0300

    refactor: change svg position

--- src/layout/sections/about/card/Card.tsx
+++ src/layout/sections/about/card/Card.tsx
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 import { Icon } from '../../../../components/icon/Icon';
 import { theme } from "../../../../styles/Theme";
 import { font } from "../../../../styles/Common";
+import svgSprite from "../../../../assets/images/icons_sprite.svg"
 
 type CardPropsType = {
     position: string,
@@ -10,8 +11,9 @@ type CardPropsType = {
     organization: string,
     city?: string,
     period: string,
-    display?: "none",
-    margin?: string
+    display?: string,
+    margin?: string,
+    iconId?: string
 }
 
 export const Card = (props: CardPropsType) => {
@@ -23,20 +25,9 @@ export const Card = (props: CardPropsType) => {
             </CardContainerTop>
 
             <CardContainerBottom>
-                <InfoContainer>
-                    <Icon iconId={"building"} height="12" width="16" viewBox="0 0 16 12" />
-                    <Info>{props.organization || "null"}</Info>
-                </InfoContainer>
-
-                <InfoContainer display={props.display}>
-                    <Icon iconId={"map"} height="12" width="16" viewBox="0 0 16 12" />
-                    <Info>{props.city}</Info>
-                </InfoContainer>
-
-                <InfoContainer>
-                    <Icon iconId={"calendar"} height="12" width="16" viewBox="0 0 16 12" />
-                    <Info>{props.period || "null"}</Info>
-                </InfoContainer>
+                <Info iconId="building">{props.organization || "null"}</Info>
+                <Info iconId="map" display={props.display} >{props.city}</Info>
+                <Info iconId="calendar">{props.period || "null"}</Info>
             </CardContainerBottom>
         </CardStyle>
     )
@@ -62,52 +53,61 @@ const CardContainerBottom = styled.div`
     justify-content: space-between;
     flex-wrap: wrap;
 
-    & :nth-of-type(3){
-        justify-content: flex-end;
-    }
-
     @media ${theme.media.mobile} {
         margin-top: 10px;
         flex-direction: column;
         gap: 10px;
-
-        & :nth-of-type(3){
-            justify-content: flex-start;
-        }
     }
 `
 
-const InfoContainer = styled.div<{display?: "none"}>`
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    flex: 1 1 33%;
-    color: ${theme.colors.font.lightContent};
-
-    ${props => props.display === "none" && css`
-        display: none;
-    `}
-`
-
-const Info = styled.span`
+const Info = styled.span<{iconId?: string, display?: string}>`
     font-weight: 500;
     font-size: 1.2rem;
     letter-spacing: 0.08em;
     color: ${theme.colors.font.lightContent};
+    padding-left: 20px;
+    display: flex;
+    align-items: center;
+
+    position: relative;
+
+    
+    &:nth-of-type(1) {
+        flex: 1 0 35%;
+    }
+    &:nth-of-type(2) {
+        flex: 6 0 20%;
+        display: ${props => props.display};
+    }
+    &:nth-of-type(3) {
+        flex: 1 -1 20%;
+
+    }
+
+    &::before {
+        content: "";
+        top: 50%;
+        left: 0;
+        transform: translate(0, -50%);
+        width: 16px;
+        height: 12px;
+        mask-image: ${props => `url(${svgSprite}#${props.iconId})`};
+        background-color: currentColor;
+        color: ${theme.colors.font.lightContent};
+
+        position: absolute;
+    }
 `
 
 const Position = styled.h4`
     ${font({weight: 400, Fmax: 20, Fmin: 16})};
-    /* font-weight: 400;
-    font-size: 2rem; */
     line-height: 1.4;
     letter-spacing: 0.05em;
 
     @media ${theme.media.mobile} {
         flex-basis: 70%;
     }
-    `
-
+`
 const Occupation = styled.span`
     border-radius: 100px;
     width: 84px;
