ea42e9284d9e213e01f1ae87d6cc84bae7bff5c3
matveev96 <sasha.matveev1996@icloud.com>
2024-10-26 23:33:00 +0300

    feat: add meia to flex wrapper

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 type FlexWrapperPropsType = {
     direction?: string,
     justfy?: string,
+    tabletJustify?: string,
+    mobileJustify?: string,
     align?: string,
+    mobileAlign?: string,
     wrap?: string,
-    gap?: string
+    gap?: string,
 }
 
 export const FlexWrapper = styled.div<FlexWrapperPropsType>`
@@ -17,4 +21,14 @@ export const FlexWrapper = styled.div<FlexWrapperPropsType>`
     flex-wrap: ${props => props.wrap || "nowrap"};
     gap: ${props => props.gap};
     height: 100%;
+
+    @media ${theme.media.proTablet} {
+        justify-content: ${props => props.tabletJustify};
+    }
+
+    @media ${theme.media.mobile} {
+        align-items: ${props => props.mobileAlign};
+        justify-content: ${props => props.mobileJustify};
+    }
+
 `
