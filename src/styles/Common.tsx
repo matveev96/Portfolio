6763a7f7ee3efd67f855cf2d8d57371421fffad8
matveev96 <sasha.matveev1996@icloud.com>
2024-10-27 15:48:25 +0300

    refactor: add default color to props

--- src/styles/Common.tsx
+++ src/styles/Common.tsx
@@ -12,7 +12,7 @@ type FontPropsType = {
 export const font = ({family, weight, color, lineHeight, Fmin, Fmax}: FontPropsType) => `
     font-family: ${family || 'Poppins'};
     font-weight: ${weight || 400};
-    color: ${color || theme.colors.font};
+    color: ${color || theme.colors.font.black};
     line-height: ${lineHeight || 1.2};
     font-size: calc((100vw - 576px)/(1600 - 576) * (${Fmax} - ${Fmin}) + ${Fmin}px);
 
