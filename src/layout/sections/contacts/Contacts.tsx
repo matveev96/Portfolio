86f63e3ab98b446d934a5be7a93e46a4e10a8251
matveev96 <sasha.matveev1996@icloud.com>
2024-10-27 15:51:19 +0300

    refactor: contacts section

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -3,38 +3,33 @@ import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Logo } from "../../../components/logo/Logo";
-import { Social } from "../../../components/social/Social";
+import { Socials } from "../../../components/socials/Socials";
 import { theme } from "../../../styles/Theme";
 import { Container } from "../../../components/Container";
+import { font } from "../../../styles/Common";
 
 export const Contacts = () => {
     return (
         <StyledContacts id="4">
-            <Container>
-                <FlexWrapper direction="column" justfy="center" align="center" gap="10px">
-                    <SectionTitle family="DM Sans, sans-serif" fontSize="5.8rem" color={theme.colors.font.contactsTitle}>For any questions please mail me:</SectionTitle>
+            <Container align="center">
+                    <SectionTitle family="DM Sans, sans-serif" fontSize={58} color={theme.colors.font.contactsTitle}>For any questions please mail me:</SectionTitle>
                     <StyledLink href="mailto:hi@pavanmg.in&body=Hello?subject=Question">hi@pavanmg.in</StyledLink>
-                </FlexWrapper>
             </Container>
         </StyledContacts>
     )
 }
 
 const StyledContacts = styled.section`
-    height: 100vh;
-    display: flex;
-    margin-bottom: 0;
 `
 
 const StyledLink = styled.a`
-    display: block;
-    font-family: DM Sans, sans-serif;
-    text-align: center;
-    font-size: 5.8rem;
-    font-weight: 700;
+    ${font({family: 'DM Sans, sans-serif',weight: 700, Fmax: 58, Fmin: 36, })};
     background: ${theme.colors.font.gradient};
     background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+    @media ${theme.media.mobile} {
+        font-size: 2.8rem;
+    }
 `
 
