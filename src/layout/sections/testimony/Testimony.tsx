24b77515ee996081064e074cf65a24914db81414
matveev96 <sasha.matveev1996@icloud.com>
2024-10-30 18:18:59 +0300

    refactor: add Slider component

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -7,19 +7,14 @@ import { theme } from "../../../styles/Theme";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { SectionSubtitle } from "../../../components/SectionSubtitle";
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
     return (
-        <StyledTestimony>  
+        <StyledTestimony id="testimony">  
             <Container>
                 <SectionTitle align="center" fontSize={48} color={theme.colors.font.darkTitle} marginBtm="50px" marginBtmTablet="20px">Testimony</SectionTitle>
                 <SectionSubtitle fontSize={32} align="center" color={theme.colors.font.darkContent} marginBtm="124px" marginBtmTablet="50px">Reviews of my work</SectionSubtitle>
                 <FlexWrapper justfy="center" align="center" direction="column" gap="40px">
                     <Slider/>
-                    <ButtonsBox>
-                        <Dot/>
-                        <Dot/>
-                        <Dot/>
-                    </ButtonsBox>
                 </FlexWrapper>
             </Container>
 
@@ -28,23 +23,4 @@ export const Testimony = () => {
 }
 
 const StyledTestimony = styled.section`
-`
-
-const ButtonsBox = styled.div`
-    display: flex;
-    gap: 10px;
-`
-
-const Dot = styled.span`
-    display: inline-block;
-    width: 10px;
-    height: 10px;
-    border-radius: 100%;
-    background-color: ${theme.colors.font.lightTitle};
-    cursor: pointer;
-    transition: .2s;
-
-    &:hover {
-        background-color: ${theme.colors.font.darkContent};
-    }
 `
