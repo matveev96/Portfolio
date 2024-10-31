879947c44c0d4a60c28ee8f4b35fb4a9c0a62aa1
matveev96 <sasha.matveev1996@icloud.com>
2024-11-01 00:03:41 +0300

    feat: add Alice slider in Testimony section

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,55 +1,53 @@
-import React from "react";
-import { SectionText } from "../SectionText";
-import { Icon } from '../icon/Icon';
-import { theme } from "../../styles/Theme";
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { S } from "./Slider_Styles";
+import { SectionText } from '../SectionText';
+import { theme } from '../../styles/Theme';
+import { Icon } from '../icon/Icon';
+import dude from "../../assets/images/Dude.webp";
+import dudeNext from "../../assets/images/Dude_2.webp";
+import girl from "../../assets/images/girl.webp";
+import './slider.css'
 
-const starsData = [
-    {
-        color: theme.colors.starYellow,
-    },
-    {
-        color: theme.colors.starYellow,
-    },
-    {
-        color: theme.colors.starYellow,
-    },
-    {
-        color: theme.colors.starYellow,
-    },
-    {
-        color: theme.colors.starYellow,
-    },
-]
+type SlidePropsType = {
+    userName?: string,
+    text?: string,
+    userPhoto?: string,
+} 
 
-export const Slider: React.FC = () => {
+const Slide = (props: SlidePropsType) => {
     return (
-        <S.Slider>
-            <S.SliderContent>
-                <S.ImageWrapper/>
-                <S.UserName>Adams Ademola</S.UserName>
-                <S.ClientAssessment>Happy Client</S.ClientAssessment>
-                <SectionText marginTop="20px" marginBtm="12px">“Everyone working in the office is very knowledgeable about all types of dental work and options for your individual needs.”</SectionText>
+        <S.SliderContent>
+            <S.PhotoWrapper userPhoto={props.userPhoto}/>
+            <S.UserName>{props.userName} </S.UserName>
+            <S.ClientAssessment>Happy Client</S.ClientAssessment>
+            <SectionText marginTop="20px" marginBtm="12px">{props.text}</SectionText>
                 <S.Stars>
-
-                    {starsData.map((s,index) => {
-                        return <Icon 
-                        key={index}
-                        iconId={"star"} 
-                        viewBox={"0 0 26 24"}  
-                        color={s.color} 
-                        width={"26px"} 
-                        height={"26px"} />
-                    })}
-
+                    <Icon iconId={"star"} viewBox={"0 0 26 24"}  color={theme.colors.starYellow} width={"26px"} height={"26px"} />
+                    <Icon iconId={"star"} viewBox={"0 0 26 24"}  color={theme.colors.starYellow} width={"26px"} height={"26px"} />
+                    <Icon iconId={"star"} viewBox={"0 0 26 24"}  color={theme.colors.starYellow} width={"26px"} height={"26px"} />
+                    <Icon iconId={"star"} viewBox={"0 0 26 24"}  color={theme.colors.starYellow} width={"26px"} height={"26px"} />
+                    <Icon iconId={"star"} viewBox={"0 0 26 24"}  color={theme.colors.starYellow} width={"26px"} height={"26px"} />
                 </S.Stars>
-            </S.SliderContent>
-            <S.ButtonsBox>
-                <S.Dot/>
-                <S.Dot/>
-                <S.Dot/>
-            </S.ButtonsBox>
-        </S.Slider>
+        </S.SliderContent>
     )
+
 }
 
+const items = [
+    <Slide userPhoto={dude} userName='Adams Ademola' text='“Everyone working in the office is very knowledgeable about all types of dental work and options for your individual needs.”'/>,
+    <Slide userPhoto={dudeNext} userName='Mark Jira' text='“Everyone working in the office is very knowledgeable about all types of dental work and options for your individual needs.”'/>,
+    <Slide userPhoto={girl} userName='Janifer Erricson' text='“Everyone working in the office is very knowledgeable about all types of dental work and options for your individual needs.”'/>
+];
+
+export const Slider = () => (
+    <S.Slider>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+            disableButtonsControls
+        />
+    </S.Slider>
+    
+);
--- src/components/slider/Slider_Styles.ts
+++ src/components/slider/Slider_Styles.ts
@@ -1,22 +1,54 @@
 import { font } from "../../styles/Common";
 import svgSprite from "../../assets/images/icons_sprite.svg";
 import styled from "styled-components";
-import dude from "../../assets/images/Dude.webp";
+
 import { theme } from "../../styles/Theme";
 
 const Slider = styled.div`
     max-width: 726px;
     width: 100%;
-    
     background-color: ${theme.colors.pageBg.lightMode};
     border-radius: 140px 0;
     box-shadow: 40px 40px 200px 0 rgba(0, 0, 0, 0.2);
 
     position: relative;
 
+    cursor: grab;
+
+    &:active {
+        cursor: grabbing;
+    }
+    
     @media ${theme.media.mobile} {
         border-radius: 80px 0;
     }
+`
+
+const SliderContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 52px 57px;
+
+    @media ${theme.media.mobile} {
+        margin: 32px 37px;
+    }
+`
+
+export type PhotoWrapperPropsType = {
+    userPhoto?: string
+}
+
+const PhotoWrapper = styled.div<PhotoWrapperPropsType>`
+    width: 104px;
+    height: 104px;
+    border: 1px solid ${theme.colors.font.black};
+    border-radius: 100px;
+    background-image: url(${props => props.userPhoto});
+    background-size: cover;
+    background-repeat: no-repeat;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
 
     &:after {
         content: "";
@@ -37,27 +69,6 @@ const Slider = styled.div`
     }
 `
 
-const SliderContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    margin: 52px 57px;
-
-    @media ${theme.media.mobile} {
-        margin: 32px 37px;
-    }
-`
-
-const ImageWrapper = styled.div`
-    width: 104px;
-    height: 104px;
-    border: 1px solid ${theme.colors.font.black};
-    border-radius: 100px;
-    background-image: url(${dude});
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-`
-
 const UserName = styled.h5`
     margin-top: 20px;
     margin-bottom: 4px;
@@ -72,36 +83,11 @@ const Stars = styled.div`
     gap: 6px;
 `
 
-const ButtonsBox = styled.div`
-    display: flex;
-    gap: 10px;
-    position: absolute;
-    bottom: -25px;
-    left: 50%;
-    transform: translateX(-50%);
-`
-
-const Dot = styled.span`
-    display: inline-block;
-    width: 10px;
-    height: 10px;
-    border-radius: 100%;
-    background-color: ${theme.colors.font.lightTitle};
-    cursor: pointer;
-    transition: .2s;
-
-    &:hover {
-        background-color: ${theme.colors.font.darkContent};
-    }
-`
-
 export const S = {
     Slider,
     SliderContent,
-    ImageWrapper,
+    PhotoWrapper,
     UserName,
     ClientAssessment,
-    Stars,
-    ButtonsBox,
-    Dot
+    Stars
 }
