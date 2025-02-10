f7d93c661fe38accabef2329c98e495d505dbf24
matveev96 <sasha.matveev1996@icloud.com>
2025-02-10 23:37:14 +0300

    refactor: comment testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -9,16 +9,17 @@ import { SectionSubtitle } from "../../../components/SectionSubtitle";
 
 export const Testimony: React.FC = () => {
     return (
-        <StyledTestimony id="testimony">  
-            <Container>
-                <SectionTitle align="center" fontSize={48} color={theme.colors.font.darkTitle} marginBtm="50px" marginBtmTablet="20px">Testimony</SectionTitle>
-                <SectionSubtitle fontSize={32} align="center" color={theme.colors.font.darkContent} marginBtm="124px" marginBtmTablet="50px">Reviews of my work</SectionSubtitle>
-                <FlexWrapper justfy="center" align="center" direction="column" gap="40px">
-                    <Slider/>
-                </FlexWrapper>
-            </Container>
-
-        </StyledTestimony>
+        <></>
+        // <StyledTestimony id="testimony">
+        //     <Container>
+        //         <SectionTitle align="center" fontSize={48} color={theme.colors.font.darkTitle} marginBtm="50px" marginBtmTablet="20px">Testimony</SectionTitle>
+        //         <SectionSubtitle fontSize={32} align="center" color={theme.colors.font.darkContent} marginBtm="124px" marginBtmTablet="50px">Reviews of my work</SectionSubtitle>
+        //         <FlexWrapper justfy="center" align="center" direction="column" gap="40px">
+        //             <Slider/>
+        //         </FlexWrapper>
+        //     </Container>
+        //
+        // </StyledTestimony>
     )
 }
 
