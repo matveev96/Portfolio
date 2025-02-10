e75f4ac969c84eb97d97f3a9b3fb2061db17672e
matveev96 <sasha.matveev1996@icloud.com>
2025-02-10 23:35:59 +0300

    refactor: add actual information

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -19,8 +19,8 @@ export const Footer: React.FC = () => {
                     <S.InfoBox>
                         <Logo color={theme.colors.font.darkTitle}/>
                         <S.ContactsContainer>
-                            <S.Link href="tel:+911234509876">+91 12345 09876</S.Link>
-                            <S.Link href="mailto:info@example.com&body=Hello?subject=Question">info@example.com</S.Link>
+                            <S.Link href="tel:+375333709381">+375 33 370-93-81</S.Link>
+                            <S.Link href="mailto:sasha.matveev1996@gmail.com&body=Hello?subject=Question">sasha.matveev1996@gmail.com</S.Link>
                             <Socials colorSVG={theme.colors.font.darkTitle} hoverColor={theme.colors.font.darkContent}/>
                         </S.ContactsContainer>
                     </S.InfoBox>
@@ -28,7 +28,7 @@ export const Footer: React.FC = () => {
                     <S.InfoBox>
                         <MenuDesktop weight="400" size="1.8rem" colorMask={theme.colors.font.darkTitle} colorMaskLine={theme.colors.font.contactsTitle} colorMaskHover={theme.colors.font.contactsTitle}/>
                         <S.Copyright>
-                            <S.ColorText>Designed and built by </S.ColorText>Pavan MG<S.ColorText> with </S.ColorText>Love<S.ColorText> & </S.ColorText>Coffee
+                            <S.ColorText>Designed and built by </S.ColorText>AlexMt<S.ColorText> with </S.ColorText>Love<S.ColorText> & </S.ColorText>Coffee
                         </S.Copyright>
                     </S.InfoBox>
                     
